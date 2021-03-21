a53ef383a5d7d97ab9a0123eb116a1571c4a86e1
Shraddha Gulati <interact.shraddha@gmail.com>
2021-03-21 22:53:08 +0530

    Added Course Files

---
+++ ApiAppDB/src/main/java/CoursesAPI/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package CoursesAPI.repository;
+
+import CoursesAPI.DTO.Course;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course,String> {
+
+
+
+
+
+}
