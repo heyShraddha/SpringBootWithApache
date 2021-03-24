b5614bd69df508b83d6c017eaadb5cc560bed9fb
Shraddha Gulati <interact.shraddha@gmail.com>
2021-03-24 22:41:36 +0530

    Added Subject

---
+++ ApiAppDB/src/main/java/SubjectAPI/controller/CourseController.java
@@ -0,0 +1,49 @@
+package CoursesAPI.controller;
+
+import CoursesAPI.DTO.Course;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    private CoursesAPI.service.CourseService CourseService;
+
+    public CourseController(CoursesAPI.service.CourseService CourseService) {
+        this.CourseService = CourseService;
+    }
+
+    //Display all the values
+    @RequestMapping("/courses")
+    public List<Course> getAllCourses()
+    {
+        return CourseService.getAllCourses();
+    }
+
+    //Display a single entity
+    @RequestMapping("/topics/{id}/courses")
+    public Course getCourse(@PathVariable String id)
+    {
+        return CourseService.getCourse(id);
+    }
+
+    //Insert Operation
+    @RequestMapping(method = RequestMethod.POST,value ="/courses")
+    public void addCourse(@RequestBody Course course)
+    {
+        CourseService.addCourse(course);
+    }
+    //update operation
+    @RequestMapping(method = RequestMethod.PUT,value ="/topics/courses/{id}")
+    public void updateCourse(@RequestBody Course course, @PathVariable String id )
+    {
+        CourseService.updateCourse(course,id);
+    }
+    //delete operation
+    @RequestMapping(method = RequestMethod.DELETE,value ="/courses/{id}")
+    public void deleteCourse(@RequestBody Course course, @PathVariable String id )
+    {
+        CourseService.deleteCourse(course,id);
+    }
+}
---
+++ ApiAppDB/src/main/java/SubjectAPI/service/CourseService.java
@@ -0,0 +1,72 @@
+package CoursesAPI.service;
+
+import CoursesAPI.DTO.Course;
+import CoursesAPI.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CourseService {
+
+
+    @Autowired
+    private CourseRepository CourseRepository;
+
+    //defining the list of Topics to perform add, delete and update operations
+    private List<Course> courses = new ArrayList<>();
+
+    //function to get all the courses displayed
+    public List<Course> getAllCourses()
+    {
+        return courses;
+//        List<Course> course = new ArrayList<>();
+//        CourseRepository.findAll() //iterating over all the course in database
+//        .forEach(course::add);
+//        return course;
+    }
+
+    //function to get a single topic through id
+    public Course getCourse(String topicId)
+    {
+        return courses.stream().filter(t -> t.getId().equals(topicId)).findFirst().get();
+//        return CourseRepository.findById(topicId).get();
+
+    }
+
+    //function to add a course of Course type to the list
+    public String addCourse(Course course)
+    {
+        courses.add(course);
+        return "addition of the course is done";
+//        CourseRepository.save(course);
+    }
+
+    //function to update a particular course using id
+    public void updateCourse(Course course, String id)
+    {
+        for(int i=0;i<courses.size();i++)
+        {
+            //comparison
+            if(courses.get(i).getId().toString().equals(id) )
+            {
+                courses.set(i,course);
+                return;
+            }
+        }
+
+//        CourseRepository.save(course);
+    }
+
+    //function to delete a particular course using id
+    public void deleteCourse(Course course, String id)
+    {
+
+        courses.removeIf(t-> t.getId().equals(id));
+//        CourseRepository.delete(course);
+    }
+
+
+}
